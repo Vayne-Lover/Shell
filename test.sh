23d1907e532c12dcb4a7b591eafd49501314e996
Vayne-Lover <406378362@qq.com>
2016-12-04 16:39:16 +0800

    Test environment variables.

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 bash
-ps --l
+ps -l
 (sleep 10)&
-coproc { sleep 10;echo $BASH_SUBSHELL;sleep 5; }
+coproc { sleep 10;echo $BASH_SUBSHELL;sleep 6; }
+echo $TEST_VARIABLE
+TEST_VARIABLE=HELLO
+echo $TEST_VARIABLE
+export my_test_variable
+unset my_test_variable
