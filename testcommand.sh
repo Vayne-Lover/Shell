3a38578d2a1ca4df760bac198be0b0c0c879a313
Vayne-Lover <406378362@qq.com>
2016-12-10 21:17:12 +0800

    Use vim annotation.

--- testcommand.sh
+++ testcommand.sh
@@ -6,3 +6,16 @@ echo "Not null."
 else
 echo "Null." 
 fi
+a=1
+b=2
+if [$a -gt $b]
+then
+echo "$a is great than $b"
+elif [$a -eq $b]
+then
+echo "$a is equal to $b"
+else
+echo "$a is less than $b"
+fi
+#9,19s/^/#/g
+#0,19s/^#//g
