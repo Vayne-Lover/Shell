5d20d53a24d8b0997536b4f95189271165aecd3c
Vayne-Lover <406378362@qq.com>
2016-12-10 22:05:18 +0800

    Test value and string.

--- testcommand.sh
+++ testcommand.sh
@@ -6,16 +6,38 @@
 #else
 #echo "Null." 
 #fi
-a=3
-b=2
-if [ $a -gt $b ]
-then
-    echo "$a is great than $b"
-elif [ $a -eq $b ]
-then
-    echo "$a is equal to $b"
-else
-    echo "$a is less than $b"
-fi
+#a=3
+#b=2
+#if [ $a -gt $b ]
+#then
+#    echo "$a is great than $b"
+#elif [ $a -eq $b ]
+#then
+#    echo "$a is equal to $b"
+#else
+#    echo "$a is less than $b"
+#fi
 #9,19s/^/#/g
 #0,19s/^#//g
+#a='abc'
+#b='cde'
+#c=''
+#if [ -z $c ]
+#then 
+#    echo "Zero."
+#else
+#    echo "Not zero."
+#fi
+a='abc'
+b='cde'
+if [ $a \> $b ]
+then 
+    echo "$a > $b."
+else
+    echo "$a < $b."
+fi
+
+
+
+
+
