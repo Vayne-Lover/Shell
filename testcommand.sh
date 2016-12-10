56a4ac1eae17cff54a54905d3030a28a4b86ae93
Vayne-Lover <406378362@qq.com>
2016-12-10 21:39:01 +0800

    Fix bugs that we should use [ a -le b ].

--- testcommand.sh
+++ testcommand.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-var1="a"
-if test $var1
-then
-echo "Not null."
-else
-echo "Null." 
-fi
-a=1
+#var1="a"
+#if test $var1
+#then
+#echo "Not null."
+#else
+#echo "Null." 
+#fi
+a=3
 b=2
-if [$a -gt $b]
+if [ $a -gt $b ]
 then
-echo "$a is great than $b"
-elif [$a -eq $b]
+    echo "$a is great than $b"
+elif [ $a -eq $b ]
 then
-echo "$a is equal to $b"
+    echo "$a is equal to $b"
 else
-echo "$a is less than $b"
+    echo "$a is less than $b"
 fi
 #9,19s/^/#/g
 #0,19s/^#//g
