693b4dd3caf225fd07b5a7d538d3379b6c398a89
Vayne-Lover <406378362@qq.com>
2016-12-11 14:55:29 +0800

    Use -d and -nt -ot -e -w -f -x.

--- ifthen.sh
+++ ifthen.sh
@@ -8,11 +8,23 @@
 #else 
 #echo "All doesn't work."
 #fi
-if [ -d ~/Desktop/CS/Shell ]
-then
-    echo "Not null."
-    cd ~/Desktop/CS/Shell
-    ls
+#if [ -d ~/Desktop/CS/Shell ]
+#then
+#    echo "Not null."
+#    cd ~/Desktop/CS/Shell
+#    ls
+#else
+#    echo "Null."
+#fi
+#if [ -e ~/Desktop/CS/Shell/ifthen.sh ]
+#then 
+#    echo "Have it."
+#else
+#    echo "Don't have."
+#fi
+if [ -r ~/Desktop/CS/Shell/ifthen.sh ]
+then 
+    echo "Can read it."
 else
-    echo "Null."
+    echo "Can't read it."
 fi
