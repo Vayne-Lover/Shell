626f134feaa7cb218a3154f6ee578fac1ddc1a04
Vayne-Lover <406378362@qq.com>
2016-12-11 14:32:13 +0800

    Use file test.

--- ifthen.sh
+++ ifthen.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-if pwd
+#if pwd
+#then
+#echo "pwd worked."
+#elif ls -al
+#then
+#echo "ls worked."
+#else 
+#echo "All doesn't work."
+#fi
+if [ -d ~/Desktop/CS/Shell ]
 then
-echo "pwd worked."
-elif ls -al
-then
-echo "ls worked."
-else 
-echo "All doesn't work."
+    echo "Not null."
+    cd ~/Desktop/CS/Shell
+    ls
+else
+    echo "Null."
 fi
