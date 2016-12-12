77bcad1c689ef41e7397d5ffefeb6a1d4ba839eb
Vayne-Lover <406378362@qq.com>
2016-12-12 21:06:48 +0800

    Use IFS.

--- for.sh
+++ for.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-list="A a B b C D E"
-list=$list" F f"
-for i in $list 
+#list="A a B b C D E"
+#list=$list" F f"
+#for i in $list 
+#do
+#    echo Don\'t say $i,"there'll" be fine.
+#done
+file1="status"
+file2="type"
+IFS_OLD=$IFS
+IFS=$':'';'','
+for i in $(cat $file2)
 do
-    echo Don\'t say $i,"there'll" be fine.
+    echo $i
 done
+for i in $(cat $file1)
+do
+    echo $i
+done
+IFS=$IFS_OLD
